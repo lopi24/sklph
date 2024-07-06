663d3d57bb2c19b62363e546d602439d16ed90bf
lopi24 <louelp24@gmail.com>
2024-07-06 13:38:47 +0800

    added adapter in mongoDB

--- auth.ts
+++ auth.ts
@@ -1,16 +1,12 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
 import User from "@/models/user.model";
 import { connectToDB } from "@/lib/database/index";
 import authConfig from "@/auth.config";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "./lib/database/db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  // providers: [
-  // Google({
-  //   clientId: process.env.GOOGLE_CLIENT_ID as string,
-  //   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-  // }),
-  // ],
+  adapter: MongoDBAdapter(clientPromise) as any,
   callbacks: {
     async session({ session }) {
       // store the user id from MongoDB to session
