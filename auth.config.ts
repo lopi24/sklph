eb7a9a95085ea5954f8213381c4b84d1565a5568
lopi24 <louelp24@gmail.com>
2024-07-06 18:26:18 +0800

    try

--- auth.config.ts
+++ auth.config.ts
@@ -2,7 +2,7 @@ import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 
 export default {
-  secret: process.env.AUTH_SECRET,
+  // secret: process.env.AUTH_SECRET,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
--- middleware.ts
+++ middleware.ts
@@ -1,44 +1,45 @@
-import NextAuth from "next-auth";
-import authConfig from "@/auth.config";
+// import NextAuth from "next-auth";
+// import authConfig from "@/auth.config";
 
-import {
-  apiAuthRoute,
-  authRoutes,
-  charityRoutes,
-  DEFAULT_LOGIN_REDIRECT,
-  DEFAULT_REDIRECT,
-  DRAFT_REDIRECT,
-  publicRoutes,
-} from "@/routes";
+// import {
+//   apiAuthRoute,
+//   authRoutes,
+//   charityRoutes,
+//   DEFAULT_LOGIN_REDIRECT,
+//   DEFAULT_REDIRECT,
+//   DRAFT_REDIRECT,
+//   publicRoutes,
+// } from "@/routes";
 
-export const { auth } = NextAuth(authConfig);
+// export const { auth } = NextAuth(authConfig);
 
-export default auth(async (req) => {
-  const { nextUrl } = req;
-  //   console.log(nextUrl);
-  const isLoggedIn = !!req.auth;
+// export default auth(async (req) => {
+//   const { nextUrl } = req;
+//   //   console.log(nextUrl);
+//   const isLoggedIn = !!req.auth;
 
-  // -- routes
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-  const isCharityRoutes = nextUrl.pathname.startsWith(charityRoutes);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthRoute);
+//   // -- routes
+//   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+//   const isCharityRoutes = nextUrl.pathname.startsWith(charityRoutes);
+//   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+//   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthRoute);
 
-  if (isApiAuthRoute) return;
+//   if (isApiAuthRoute) return;
 
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-    }
-    return;
-  }
+//   if (isAuthRoute) {
+//     if (isLoggedIn) {
+//       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+//     }
+//     return;
+//   }
 
-  if (!isLoggedIn && !isPublicRoute && !isCharityRoutes) {
-    return Response.redirect(new URL(DEFAULT_REDIRECT, nextUrl));
-  }
+//   if (!isLoggedIn && !isPublicRoute && !isCharityRoutes) {
+//     return Response.redirect(new URL(DEFAULT_REDIRECT, nextUrl));
+//   }
 
-  return;
-});
+//   return;
+// });
+export { auth as middleware } from "@/auth";
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
